b607620bdfabf2e63bf5c3ed4628a26995d8d398
magnummoran <58849222+magnummoran@users.noreply.github.com>
2022-11-08 11:28:54 -0500

    Add files via upload

---
+++ Script that actually works.ps1
@@ -0,0 +1,14 @@
+﻿Get-Content U:\WINDOWS\users.txt | ForEach-Object {
+  $mail = $_
+  $user = Get-ADUser -LDAPFilter "(mail=$mail)"
+  if ( $user ) {
+    $sAMAccountName = $user.sAMAccountName
+  }
+  else {
+    $sAMAccountName = $null
+  }
+  [PSCustomObject] @{
+    "sAMAccountName" = $sAMAccountName
+  }
+} | Export-Csv U:\WINDOWS\Folder\users1.txt -NoTypeInformation
+#This script takes the text file, grabs the emails from the file and then creates a files that returns email with login ID 09/4/2020#
---
+++ Script that actually workspart2.ps1
@@ -0,0 +1,3 @@
+﻿Get-Content U:\PowershellScriptFolder\Folder\users1.txt |
+	Foreach-Object {Add-ADGroupMember -Identity NL_SG_Insurance_Matrix_RO -Members $_}
+#This script gets the content from file and then adds each object in the file to the security group#
